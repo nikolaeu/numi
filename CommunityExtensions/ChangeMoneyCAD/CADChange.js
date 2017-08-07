97f313be90e6768aaedd38c6aba810fb50e27626
Benjamin Chausse <BCPianist@users.noreply.github.com>
2017-08-07 11:15:52 -0400

    Incorporate a community extension directory (#49)

---
+++ CommunityExtensions/ChangeMoneyCAD/CADChange.js
@@ -0,0 +1,39 @@
+numi.addUnit({
+    "id": "FiveCents",
+    "phrases": "nickel, Nickel, nickels, Nickels, FiveCent, FiveCents",
+    "baseUnitId": "CAD",
+    "format" : "Nickels",
+    "ratio" : 0.05,
+});
+
+numi.addUnit({
+    "id": "TenCents",
+    "phrases": "dime, Dime, dimes, Dimes, TenCent, TenCents",
+    "baseUnitId": "CAD",
+    "format" : "Dimes",
+    "ratio" : 0.1,
+});
+
+numi.addUnit({
+    "id": "TwentyFiveCents",
+    "phrases": "quarter, Quarter, quarters, Quarters, TwentyFiveCent, TwentyFiveCents",
+    "baseUnitId": "CAD",
+    "format" : "Quarters",
+    "ratio" : 0.25,
+});
+
+numi.addUnit({
+    "id": "OneDollars",
+    "phrases": "loonie, Loonie, loonies, Loonies, OneDollar, OneDollars",
+    "baseUnitId": "CAD",
+    "format" : "Loonies",
+    "ratio" : 1,
+});
+
+numi.addUnit({
+    "id": "TwoDollars",
+    "phrases": "toonie, Toonie, toonies, Toonies, TwoDollar, TwoDollars",
+    "baseUnitId": "CAD",
+    "format" : "Toonies",
+    "ratio" : 2,
+});
---
+++ CommunityExtensions/ChangeMoneyCAD/README.md
@@ -0,0 +1,28 @@
+# Canadian Money Change Extension for Numi :tada:
+
+
+## What is this extension for? :mag_right:
+
+This extension allows you to easily calculate change (in canadian money) using the standard terminology. It can also be used for informative purposes (for example, how many Nickels do you need to reach 10 000$ CAD).
+
+
+## Installation :floppy_disk:
+
+Simply download the .js file to your numi extensions directory.
+
+
+## How to use it :wrench:
+
+As mentioned above simply use the canadian change terminology. This includes:
+
+- Nickels  (0.05$ CAD)
+- Dimes    (0.10$ CAD)
+- Quarters (0.25$ CAD)
+- Loonies  (1.00$ CAD)
+- Toonies  (2.00$ CAD)
+
+The terminology described above can be used with first letter capital or not. Also, plural and singular are recognized.
+
+
+## Example :memo:
+![extensionDemonstration](https://raw.githubusercontent.com/BCPianist/numi-extensions/master/CommunityExtensions/ChangeMoneyCAD/SS.png)
