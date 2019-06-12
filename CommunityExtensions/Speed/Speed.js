9830bbd595019f6d1d04f7e6d74972121ca6441e
Aleksandar Gosevski <gosevskibg@gmail.com>
2019-06-12 16:51:44 +0200

    Add speed extension

---
+++ CommunityExtensions/Speed/Speed.js
@@ -0,0 +1,15 @@
+numi.addUnit({
+    "id": "kmh",
+    "phrases": "kmh, kmph, km/h, khm",
+    "baseUnitId": "second",
+    "format": "kmh",
+    "ratio": 1
+});
+
+numi.addUnit({
+    "id": "mph",
+    "phrases": "mph, MPH, miles per hour",
+    "baseUnitId": "second",
+    "format": "mph",
+    "ratio": 1.609344
+});
