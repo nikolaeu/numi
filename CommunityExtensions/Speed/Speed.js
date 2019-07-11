db2ffcc9ca40037c803115381108f6b19ff88145
Alex <brewingcode@users.noreply.github.com>
2019-07-10 19:03:29 -0700

    Add m/s, ft/s, and kts to Speed extension (#117)
    Also cleaned up the phrases and formats for MPH and KM/H: phrases cannot
    have slashes, and formats are more standard.

--- CommunityExtensions/Speed/Speed.js
+++ CommunityExtensions/Speed/Speed.js
@@ -1,15 +1,39 @@
 numi.addUnit({
     "id": "kmh",
-    "phrases": "kmh, kmph, km/h, khm",
+    "phrases": "kmh, kmph, khm, kph, klicks, kilometers per hour",
     "baseUnitId": "second",
-    "format": "kmh",
+    "format": "km/h",
     "ratio": 1
 });
 
 numi.addUnit({
     "id": "mph",
-    "phrases": "mph, MPH, miles per hour",
+    "phrases": "mph, miles per hour",
     "baseUnitId": "second",
     "format": "mph",
     "ratio": 1.609344
+});
+
+numi.addUnit({
+    "id": "meterspersecond",
+    "phrases": "mps, meters per second",
+    "baseUnitId": "second",
+    "format": "m/s",
+    "ratio": 3.6
+});
+
+numi.addUnit({
+    "id": "feetpersecond", // allow for "frames per second" elsewhere
+    "phrases": "fps, ftps, ft per sec, ft per second, feet per second",
+    "baseUnitId": "second",
+    "format": "ft/s",
+    "ratio": 1.097
+});
+
+numi.addUnit({
+    "id": "knots",
+    "phrases": "kts, knots",
+    "baseUnitId": "second",
+    "format": "kt",
+    "ratio": 1.852
 });
