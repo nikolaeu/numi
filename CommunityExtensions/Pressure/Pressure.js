dcc0fd46813107dbb45fb7c4e31f365cee7d62ca
Tim Schuitemaker <timschuitemaker@users.noreply.github.com>
2019-08-03 01:00:06 +0200

    Created Pressure extension (#119)
    * added Pressure.js
    
    * added Pressure extension

---
+++ CommunityExtensions/Pressure/Pressure.js
@@ -0,0 +1,72 @@
+
+numi.addUnit({
+    "id": "bar",
+    "phrases": "bar",
+    "baseUnitId": "m2",
+    "format": "bar",
+    "ratio": 1
+});
+
+numi.addUnit({
+    "id": "millibar",
+    "phrases": "millibar",
+    "baseUnitId": "m2",
+    "format": "millibar",
+    "ratio": 0.001
+});
+
+numi.addUnit({
+    "id": "pascal",
+    "phrases": "pascal, Pa",
+    "baseUnitId": "m2",
+    "format": "Pa",
+    "ratio": 0.00001
+});
+
+numi.addUnit({
+    "id": "kilopascal",
+    "phrases": "kilopascal, kPa",
+    "baseUnitId": "m2",
+    "format": "kPa",
+    "ratio": 0.01
+});
+
+numi.addUnit({
+    "id": "hectopascal",
+    "phrases": "hectopascal, hPa",
+    "baseUnitId": "m2",
+    "format": "hPa",
+    "ratio": 0.001
+});
+
+numi.addUnit({
+    "id": "megapascal",
+    "phrases": "megapascal, MPa",
+    "baseUnitId": "m2",
+    "format": "MPa",
+    "ratio": 10
+});
+
+numi.addUnit({
+    "id": "megapascal",
+    "phrases": "megapascal, MPa",
+    "baseUnitId": "m2",
+    "format": "MPa",
+    "ratio": 10
+});
+
+numi.addUnit({
+    "id": "newton per square meter",
+    "phrases": "newton per m2, N/m2, newton per square meter",
+    "baseUnitId": "m2",
+    "format": "MPa",
+    "ratio": 0.00001
+});
+
+numi.addUnit({
+    "id": "standard atmosphere",
+    "phrases": "atm",
+    "baseUnitId": "m2",
+    "format": "atm",
+    "ratio": 1.0132533539
+});
