0c2edeedb91ea6ca7695361d54de2abf0e72d9a2
Oliver <me@oliverlumby.com>
2021-03-29 23:19:09 +0200

    add second parameter to override vat rate

--- plugins/CommunityExtensions/NettTotalCalculator/NettCalc.js
+++ plugins/CommunityExtensions/NettTotalCalculator/NettCalc.js
@@ -3,8 +3,8 @@
 // Function to find the nett figure of a gross input amount
 numi.addFunction({ "id" : "nett", "phrases" : "nett" }, function(values) {
     
-    // Change this to whatever your tax amount is
-    var vat = 20;
+    // You can change `20` to your tax amount to set as default
+    var vat = values[1]?.double ?? 20;
     
     return { "double": (values[0].double/(vat+100)) * 100 };
 
