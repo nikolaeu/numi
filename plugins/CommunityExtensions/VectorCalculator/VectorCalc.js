d1d53b12f1a3d51c00d959c7824907cb763eb15d
Elam Cohavi <elam.cohavi@gmail.com>
2022-07-25 12:38:47 +0100

    Prevent vectorStore duplicates + restrict to 2d & 3d vectors

--- plugins/CommunityExtensions/VectorCalculator/VectorCalc.js
+++ plugins/CommunityExtensions/VectorCalculator/VectorCalc.js
@@ -1,4 +1,4 @@
-let vectors = []
+let vectorStore = [];
 
 const mag = (vec) => {
     return vec.map((x) => x * x).reduce((m, n) => m + n);
@@ -13,7 +13,7 @@ numi.addFunction({ "id": "angle", "phrases": "angle" }, function (values) {
         return;
     }
 
-    const [u, v] = [vectors[values[0].double], vectors[values[1].double]];
+    const [u, v] = [vectorStore[values[0].double], vectorStore[values[1].double]];
     return {
         "double": Math.acos(dot(u, v) / (Math.sqrt(mag(u)) * Math.sqrt(mag(v)))),
         "unitId": "radian"
@@ -25,14 +25,30 @@ numi.addFunction({ "id": "dot", "phrases": "dot" }, function (values) {
         return;
     }
 
-    const [u, v] = [vectors[values[0].double], vectors[values[1].double]];
+    const [u, v] = [vectorStore[values[0].double], vectorStore[values[1].double]];
     return v.length === u.length ? dot(u, v) : null;
 });
 
 numi.addFunction({ "id": "vec", "phrases": "vec" }, function (values) {
-    if (values.length >= 2) {
-        const i = vectors.push(values.map((obj) => obj.double));
-        return (i - 1);
+    if (values.length >= 2 && values.length <= 3) {             // allow only 2d,3d vectors
+        const argVector = values.map((obj) => obj.double);
+
+        let returnIndex = null;
+        vectorStore.every((vector, vectorIndex) => {
+            if (vector.length === argVector.length) {
+                if (argVector.every((val, i) => val === vector[i])) {
+
+                    returnIndex = vectorIndex;
+                    return false;
+                }
+            }
+            return true;
+        });
+
+        if (returnIndex === null) {
+            return (vectorStore.push(argVector) - 1);
+        }
+        return returnIndex;
     }
     return;
 });
