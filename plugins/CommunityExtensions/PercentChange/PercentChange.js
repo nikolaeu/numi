76fac639556b7a30623b0cb61ea0dd87b4cf9f2d
Nick Quinlan <nick@nicholasquinlan.com>
2022-11-18 07:57:22 -0800

    fixed divide by zero error

--- plugins/CommunityExtensions/PercentChange/PercentChange.js
+++ plugins/CommunityExtensions/PercentChange/PercentChange.js
@@ -5,5 +5,9 @@ numi.addFunction({ "id": "pc", "phrases": "pc" }, (values) => {
 
     const [initial, final] = values.map(v => v.double);
 
+    if(initial === 0) {
+        return;
+    }
+
     return { double: (final-initial)/initial*100, unitId: "percent" };
 });
