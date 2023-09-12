16b066d02c6c6ebeb7639032f8b2c3072dc3e49c
Timo <43761260+clemone210@users.noreply.github.com>
2023-09-12 11:34:30 +0200

    Basic electrical conversion plugin for Numi
    Electrical power conversion with Milliwatt, Watt, Kilowatt, Gigawatt, Megawatt.

---
+++ plugins/CommunityExtensions/Electrical-conversion/Electrical-conversion.js
@@ -0,0 +1,34 @@
+numi.addUnit({
+    "id": "milliwatt",
+    "phrases": "milliwatt, mW",
+    "baseUnitId": "watt",
+    "format": "mW",
+    "ratio": 0.001,
+});
+numi.addUnit({
+    "id": "watt",
+    "phrases": "watt, W",
+    "format": "W",
+    "ratio": 1,
+});
+numi.addUnit({
+    "id": "kilowatt",
+    "phrases": "kilowatt, kW",
+    "baseUnitId": "watt",
+    "format": "kW",
+    "ratio": 1000,
+});
+numi.addUnit({
+    "id": "megawatt",
+    "phrases": "megawatt, MW",
+    "baseUnitId": "watt",
+    "format": "MW",
+    "ratio": 1000000,
+});
+numi.addUnit({
+    "id": "gigawatt",
+    "phrases": "gigawatt, GW",
+    "baseUnitId": "watt",
+    "format": "GW",
+    "ratio": 1000000000,
+});
